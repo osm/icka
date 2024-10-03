7da4c6625fb29d6fc56935599a081110166ba95e
TehPeGaSuS <25697531+TehPeGaSuS@users.noreply.github.com>
2024-10-04 00:08:31 +0200

    Update icka.sh
    Let's make the file prettier

--- icka.sh
+++ icka.sh
@@ -1,46 +1,57 @@
 #!/usr/bin/env bash
 
-##########
-# Instructions
-##########
-# Edit the script with your account data
-# and the paths on the example commands
+#--------------#
+# Instructions #
+#--------------#
+# Edit the script replacing any occurrence of `$user` with your shell username,
+# your email, password and correct paths to the binary
 ##
 # Make this file executable with:
-#   chmod +x /home/pegasus/icka.sh
+#   chmod +x /home/$user/icka.sh
 ##
 # Add the following to your crontab, to make it
 # run every hour, so you never disconnect:
-#   0 * * * * /home/pegasus/icka.sh >/dev/null 2>&1
+#   0 * * * * /home/$user/icka.sh >/dev/null 2>&1
 ##
 # Enjoy!
-##########
+#---------------------#
+# End of instructions #
+#---------------------#
 
-##########
-# IRCCloud account email
-##########
+
+##---------------##
+## Configuration ##
+##---------------##
+
+#------------------------#
+# IRCCloud account email #
+#------------------------#
 ickaEmail="email@example.com"
 
-##########
-# IRCCloud account password
-##########
+#---------------------------#
+# IRCCloud account password #
+#---------------------------#
 ickaPassword="foobar"
 
-##########
-# Path to the icka binary
-##########
-ickaBinary="/home/pegasus/go/bin/icka"
+#-------------------------#
+# Path to the icka binary #
+#-------------------------#
+ickaBinary="/home/$user/go/bin/icka"
 
-##########
-# Name of the file where we are going to write the successfailure message?
-# Only one success/failure message will be kept every time this script is executed
-##########
+#-----------------------------------------------------------------------------------#
+# Name of the file where we are going to write the successfailure message?          #
+# Only one success/failure message will be kept every time this script is executed. #
+#-----------------------------------------------------------------------------------#
 ickaLog="icka.log"
 
-##########
-# Don't edit anything below unless you know exactly what you're doing.
-# If you touch the code below and then complain the script "suddenly stopped working" I'll touch you at night.
-##########
+##----------------------##
+## End of configuration ##
+##----------------------##
+
+#--------------------------------------------------------------------------------------------------------------#
+# DON'T EDIT ANYTHING BELOW UNLESS YOU KNOW EXACTLY WHAT YOU'RE DOING!                                         #
+# If you touch the code below and then complain the script "suddenly stopped working" I'll touch you at night. #
+#--------------------------------------------------------------------------------------------------------------#
 "$ickaBinary" -email "$ickaEmail" -password "$ickaPassword"
 
 if [ $? -eq 0 ]; then
