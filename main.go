6e38adfabb62c6853c5cffa5de857f86570739e2
Oscar Linderholm <osm@recv.se>
2025-02-05 17:51:45 +0100

    Bump default user agent string

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ import (
 	"github.com/osm/flen"
 )
 
-const userAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
+const defaultUserAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36"
 
 type AuthTokenResponse struct {
 	Token   string `json:"token"`
